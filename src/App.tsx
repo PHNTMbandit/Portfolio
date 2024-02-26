fb64c4c1f286a74e615a5c238d59128d8f858b51
Domenic Pittari <dom.pittari@gmail.com>
2024-02-26 12:12:09 +0900

    Added language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
-import { ModeToggle } from "./components/mode-toggle";
+import LanguageToggle from "./components/LanguageToggle";
+import { ModeToggle } from "./components/ModeToggle";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
-    <>
-      <ModeToggle />
-    </>
+    <div className="flex flex-col">
+      <div className="flex justify-end space-x-4">
+        <ModeToggle />
+        <LanguageToggle />
+      </div>
+      <h1 className="h1-extrabold">{t("main.header")}</h1>
+    </div>
   );
 }
 
---
+++ src/components/LanguageToggle.tsx
@@ -0,0 +1,51 @@
+import { useTranslation } from "react-i18next";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Globe } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+const locales: Record<string, { title: string }> = {
+  en: { title: "English" },
+  jp: { title: "Japanese" },
+};
+
+const LanguageToggle = () => {
+  const { i18n } = useTranslation();
+  const [position, setPosition] = useState(i18n.language);
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon">
+            <Globe className="h-[1.2rem] w-[1.2rem]" />
+            <span className="sr-only">Toggle language</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuRadioGroup
+            value={position}
+            onValueChange={setPosition}>
+            {Object.keys(locales).map((locale) => (
+              <DropdownMenuRadioItem
+                value={locale}
+                onClick={() => i18n.changeLanguage(locale)}>
+                {locales[locale].title}
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default LanguageToggle;
